62eaf471662c7e03378f050b6fe88d56d096f371
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-03-29 12:50:19 -0400

    initial setup

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using Matrix = std::vector<std::vector<double>>;
+
+void benchmarkMatrixMultiplication();
+
+int main()
+{
+    benchmarkMatrixMultiplication();
+    return 0;
+}
