f45df34681b5d093753605518ebc65c1bf114a2c
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-03-29 13:12:18 -0400

    blocking multiplication

--- main.cpp
+++ main.cpp
@@ -20,6 +20,35 @@ Matrix naiveMultiply(const Matrix& A, const Matrix& B)
     return C;
 }
 
+Matrix tiledMultiply(const Matrix& A, const Matrix& B, size_t blockSize)
+{
+    size_t n = A.size();
+    Matrix C(n, std::vector<double>(n, 0.0));
+
+    for (size_t i{ 0 }; i < n; i += blockSize)
+    {
+        for (size_t j{ 0 }; j < n; j += blockSize)
+        {
+            for (size_t k{ 0 }; k < n; k += blockSize)
+            {
+                // Perform the multiplication for the block
+                for (size_t i2{ i }; i2 < std::min(i + blockSize, n); ++i2)
+                {
+                    for (size_t j2{ j }; j2 < std::min(j + blockSize, n); ++j2)
+                    {
+                        for (size_t k2{ k }; k2 < std::min(k + blockSize, n);
+                             ++k2)
+                        {
+                            C[i2][j2] += A[i2][k2] * B[k2][j2];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return C;
+}
+
 void benchmarkMatrixMultiplication();
 
 int main()
