9a10142af554f0b0528268e1867ac3c7623c7547
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-03-29 13:07:09 -0400

    naive matrix multiplication

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 
 using Matrix = std::vector<std::vector<double>>;
 
+Matrix naiveMultiply(const Matrix& A, const Matrix& B)
+{
+    size_t n = A.size();
+    Matrix C(n, std::vector<double>(n, 0.0));
+    for (size_t i{ 0 }; i < n; ++i)
+    {
+        for (size_t j{ 0 }; j < n; ++j)
+        {
+            for (size_t k{ 0 }; k < n; ++k)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
+}
+
 void benchmarkMatrixMultiplication();
 
 int main()
